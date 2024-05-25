0ec33ce4833205a7488ae08cbd10c0854170b09c
morikentarou <routatarou1119@gmail.com>
2024-05-25 18:38:53 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   has_many :purchases
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   VALID_KANA_REGEX = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
   validates :nickname,           presence: true
   validates :email,              format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
+  validates :password,           length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
   validates :last_name,          presence: true
   validates :first_name,         presence: true
   validates :reading_last_name,  presence: true, format: { with: VALID_KANA_REGEX }
