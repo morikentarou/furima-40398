7c784336e1d3edd03a459826a9737b8fdb87e877
morikentarou <routatarou1119@gmail.com>
2024-05-30 01:18:30 +0900

    カラムとバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase
+
+  validates :image,         presence: true
+  validates :name,          presence: true
+  validates :description,   presence: true
+  validates :category_id,   presence: true
+  validates :condition_id,  presence: true
+  validates :delivery_id,   presence: true
+  validates :area_id,       presence: true
+  validates :time_id,       presence: true
+  validates :price,         presence: true
 end
