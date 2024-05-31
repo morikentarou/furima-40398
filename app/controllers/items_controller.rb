1af76beb5507296f73c2b02e230d5a5d9944ce33
morikentarou <routatarou1119@gmail.com>
2024-05-31 21:50:28 +0900

    エラーハンドリングの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(prototype_params)
-    if @prototype.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -16,8 +16,8 @@ class ItemsController < ApplicationController
   end
 
   private
-  def prototype_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :area_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_id, :area_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
 
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Furima40398
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoloader = :classic
   end
 end
