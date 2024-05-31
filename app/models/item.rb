2f76b285496eb9e14063d93e3adf56acfa5f624c
morikentarou <routatarou1119@gmail.com>
2024-05-31 16:48:18 +0900

    アクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :area
+  belongs_to :scheduled_delivery 
+  belongs_to :delivery
   belongs_to :user
   # has_one :purchase
 
   validates :image,         presence: true
   validates :name,          presence: true
   validates :description,   presence: true
-  validates :category_id,   presence: true
-  validates :condition_id,  presence: true
-  validates :delivery_id,   presence: true
-  validates :area_id,       presence: true
-  validates :time_id,       presence: true
+  validates :category_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :time_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,         presence: true
 end
