8a289355ceb4b08565c0a504e115b7e923d92cd4
morikentarou <routatarou1119@gmail.com>
2024-05-31 17:39:21 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :delivery
   belongs_to :area
   belongs_to :scheduled_delivery 
-  belongs_to :delivery
+
   belongs_to :user
   # has_one :purchase
 
@@ -16,5 +17,5 @@ class Item < ApplicationRecord
   validates :delivery_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id,               presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,                 presence: true
+  validates :price,                 presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
