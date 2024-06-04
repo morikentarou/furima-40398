875a61b6e54531dbcd22a3764c6f8a83042c9f7f
morikentarou <routatarou1119@gmail.com>
2024-06-05 04:26:31 +0900

    テストコード一部修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -92,7 +92,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
-      it 'priceが半角数値でしか登録できない' do
+      it 'priceは半角数値でしか登録できない' do
         @item.price = '１１１'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
