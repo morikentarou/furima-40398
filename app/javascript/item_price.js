691e82cfd80a9cee3d74ac2d1fc5ce3adfb91a27
morikentarou <routatarou1119@gmail.com>
2024-06-05 03:59:30 +0900

    JavaScript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
+document.addEventListener('turbo:load', function() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
 
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML =(Math.floor(inputValue * 0.1));
 
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML =(Math.floor(inputValue * 0.1));
-
-  const profitDom = document.getElementById("profit");
-  profitDom.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
-});
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
+  });
 });
