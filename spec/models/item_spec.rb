3fc7fce6ae83f4d75b53e98614b0959d03d191aa
morikentarou <routatarou1119@gmail.com>
2024-06-06 02:41:37 +0900

    rubocop 実施

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Item, type: :model do
       it 'userが空では登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it 'imagが空では登録できない' do
         @item.image = nil
