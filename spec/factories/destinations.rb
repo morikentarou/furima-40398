f895420f0fdcd90dadfc1671f62ae40dbbff5138
morikentarou <routatarou1119@gmail.com>
2024-06-18 02:23:09 +0900

    テストコード記述

--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :destination do
-    
+  association :purchase
+  post_code        { Faker::Number.number(digits: 3).to_s + '-' + Faker::Number.number(digits: 4).to_s }
+  area_id          { Faker::Number.between(from: 2, to: 48) }
+  municipality     { Faker::Lorem.characters(number: 10) }
+  street           { Faker::Lorem.characters(number: 10) }
+  building         { Faker::Lorem.characters(number: 10) }
+  tel              { Faker::Number.between(from: 10**9, to: 10**11)}
   end
 end
