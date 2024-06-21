573e75a77fc0f2a1d8b51a07227f0a58b8011280
morikentarou <routatarou1119@gmail.com>
2024-06-21 16:39:06 +0900

    rubocop 実施

--- app/models/destination_form.rb
+++ app/models/destination_form.rb
@@ -1,6 +1,7 @@
 class DestinationForm
   include ActiveModel::Model
   attr_accessor :post_code, :area_id, :municipality, :street, :building, :tel, :user_id, :item_id, :token, :price
+
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -14,8 +15,8 @@ class DestinationForm
   def save
     return false unless valid?
 
-    purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Destination.create(post_code: post_code, area_id: area_id, municipality: municipality, street: street, building: building,
-                       tel: tel, purchase_id: purchase.id).persisted?
+    purchase = Purchase.create(user_id:, item_id:)
+    Destination.create(post_code:, area_id:, municipality:, street:, building:,
+                       tel:, purchase_id: purchase.id).persisted?
   end
 end
